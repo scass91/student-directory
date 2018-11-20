9919f9fdd3c9aff5ea6dc6a2427959a614d6389e
Simon Cass <scass1991@gmail.com>
2018-11-20 16:20:08 +0000

    Added ability to lead data from .csv

--- directory.rb
+++ directory.rb
@@ -13,10 +13,21 @@ def save_students
   file.close
 end
 
+def load_students
+  # "r" refers to read only
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -34,6 +45,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
