84578d37851c03c7af718e9606ea821a7bbade4e
Simon Cass <scass1991@gmail.com>
2018-11-20 18:56:10 +0000

    Used DRY priciples to remove repeated requests for @students input

--- directory.rb
+++ directory.rb
@@ -1,8 +1,84 @@
-@students = [] # an empty array accessibly to all methods
+@students = [] # an empty array accessible to all methods
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit # this will cause the program to terminate
+  when "3"
+    save_students
+  when "4"
+    load_students
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def add_students(name, cohort)
+  @students << {name: name, cohort: cohort.to_sym}
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # get the first name
+  name = STDIN.gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    puts "Please enter a cohort: "
+    cohort = STDIN.gets.chomp
+    if cohort == ""
+      cohort = "January"
+    end
+    add_students(name, cohort)
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
+    name = STDIN.gets.chomp
+  end
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
 
 def save_students
   # open the file for writing
-  # "w" relates to write only permissions
   file = File.open("students.csv", "w")
   # iterate over the array of students
   @students.each do |student|
@@ -13,100 +89,13 @@ def save_students
   file.close
 end
 
-# students.csv is the defaut value, however now any given filename will load
-# if passed in terminal
 def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
-    name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
+  name, cohort = line.chomp.split(',')
+    add_students(name, cohort)
   end
   file.close
-end
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit" # 9 because we'll be adding more items
-end
-
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end
-
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "3"
-      save_students
-    when "4"
-      load_students
-    when "9"
-      exit
-    else
-      puts "I don't know what you mean, try again"
-  end
-end
-
-def interactive_menu
-  loop do
-    print_menu
-    process(STDIN.gets.chomp)
-  end
-end
-
-def print_header
-  puts "The students of Villains Academy".center(50)
-  puts "-------------".center(50)
-end
-
-def print_students_list
-  @students.each do |student|
-    puts "#{student[:name]} #{student[:age]} #{student[:location]} (#{student[:cohort]} cohort)"
-  end
-end
-
-def print_footer
-  puts "Overall, we have #{@students.count} great students"
-end
-
-def input_students
-  puts "Please enter the names of the students".center(50)
-  puts "To finish, just hit return twice".center(50)
-  # get the first name
-  name = STDIN.gets.chomp
-  puts "Enter a cohort for the student, to finish, just hit return twice"
-  cohort = STDIN.gets.chomp
-  if cohort == ""
-    cohort = "January"
-  end
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    # add the student hash to the array
-    @students << {name: name, cohort: cohort, age: :"27 years old,", location: :London}
-    if @students.count == 1
-       puts puts "Now we have #{@students.count} student".center(50)
-     else
-    puts "Now we have #{@students.count} students".center(50)
-    end
-    # get another name from the user
-    name = STDIN.gets.chomp
-    cohort = STDIN.gets.chomp
-    if cohort == ""
-      cohort = "January"
-    end
-  end
-  # return the array of students
-  if @students.size > 0
-    @students
-  else puts ""
-  end
 end
 
 def try_load_students
@@ -121,5 +110,5 @@ def try_load_students
   end
 end
 
-load_students
+try_load_students
 interactive_menu
