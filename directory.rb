cf0f352a925e32e95b0eb5c57a8b23e45fa4b311
Simon Cass <scass1991@gmail.com>
2018-11-20 16:08:16 +0000

    Added .csv capabilities as well .gitignore

--- directory.rb
+++ directory.rb
@@ -1,8 +1,22 @@
 @students = [] # an empty array accessibly to all methods
 
+def save_students
+  # open the file for writing
+  # "w" relates to write only permissions
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -18,6 +32,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
